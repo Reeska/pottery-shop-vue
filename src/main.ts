0677d29539bc9d4fc2a89ffe8422550d4e47182c
Reeska <thchampi@gmail.com>
2020-07-05 22:15:50 +0200

    Add pottery-ui and load web components in main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import {applyPolyfills, defineCustomElements} from '@pottery-ui/core/loader';
 
 Vue.config.productionTip = false
+Vue.config.ignoredElements = [/ui-\w*/];
+
+applyPolyfills().then(() => {
+  defineCustomElements();
+});
 
 new Vue({
   router,
